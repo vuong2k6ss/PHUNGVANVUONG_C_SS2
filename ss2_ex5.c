77072587435530ee9b7407338ff3934f283b3830
vuong2k6ss <vuongvanphung2k6@gmail.com>
2024-11-20 16:08:43 +0700

    bt5

---
+++ ss2_ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int length = 5; 
+    int width = 4; 
+    int P = (length + width) * 2; //chu vi hcn
+    int S = length * width;  // dien tich hcn    
+    printf("Chu vi hcn la: %d\n",P);
+    printf("Dien tich hcn la: %d\n",S);
+    return 0;
+}
+
