b37f631139b7bff075686e99ce1b926dd6363ed0
vuong2k6ss <vuongvanphung2k6@gmail.com>
2024-11-20 16:08:18 +0700

    bt4

---
+++ ss2_ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int side = 3; // canh hinh vuong la 3
+	int P = side * 4;//chu vi hinh vuong
+	int S = side * side; // dien tich hinh vuong
+	printf("chu vi hinh vuong la: %d\n",P);
+	printf("dien tich hinh vuong la: %d\n",S);
+	return 0;
+}
+
