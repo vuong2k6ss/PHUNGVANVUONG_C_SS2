d91432765b84558982492cfea5fb4c4d60bc4dd8
vuong2k6ss <vuongvanphung2k6@gmail.com>
2024-11-20 16:05:29 +0700

    bt1

---
+++ ss2_ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // kieu so nguyen (int)
+    int age = 18; 
+    // kieu so thuc (float)
+    float pi = 3.14; 
+    // kieu ki tu (char) luu 1 ki tu duy nhat
+    char grade = 'B'; 
+    // kieu logic (boolean) dùng de luu gia tri true or false
+	bool_A = 1; // 1 tuong duong true, 0 tuong duong voi false
+    return 0;
+}
+
