78578a83341b275168faf61a17f534e7302a5736
vuong2k6ss <vuongvanphung2k6@gmail.com>
2024-11-20 16:09:50 +0700

    bt6

---
+++ ss2_ex6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float PI = 3.14;
+	int R = 5;// ban kinh hinh tron
+	float P = 2 * PI * R;
+	float S = PI * R * R;
+	printf("chu vi hinh tron la %f\n",P);
+	printf("dien tich hinh tron la %f",S);
+	return 0;
+}
+
